e5d6ca9213b62ac5144dc1784399ead226eca344
Steve Kondik <shade@chemlab.org>
2010-12-22 10:45:26 -0500

    vidc: Update from CodeAurora
    qcom-omxcore: Quiet debug logging
    
    Change-Id: Ie46c5e2511a50e04baa9295357c66dd9d927aca4
    
    mm-video: fix VideoHeap allocations/deallocations & clean up fd usage
    
    Made VideoHeap free memory at an appropriate time, and do its own
    pmem allocation.
    
    Changed all valid fd comparisons to >= 0, so there is no need
    to needlessly open the pmem fd more than once.
    
    Change-Id: Ib325682f7df5eb8254f84ca7d3caaf60d4b91468
    
    mm-video: cleaned up omx_venc & changed fd comparisons
    
    Removed the non-used VideoHeap from omx_venc, cleaned up some
    duplicated code, and changed fd comparisons to >= 0 for valid.
    
    Change-Id: I10a0cd351461d3bc48a5f344f6111f14ea5a1a19
    
    mm-video: vidc: vdec: free output buffer pmem when all buffers are free
    
    Seems to hang on to them too long otherwise.
    
    Change-Id: I43fc678bb68784e509de74031092d3fd1bd0354f
    
    mm-video: vidc: vdec: ditch VideoHeap for plain MemoryHeapBase
    
    Looks like MemoryHeapBase does give the fd.  Oops.
    
    Change-Id: I8693e93d956267848c5a2ee1b16ddbf90ed7cf13
    
    vidc: vdec: Fix for timestamp resolution
    
    Fix to propagate the 64-bits timestamps.
    
    Change-Id: Iab596ee10a533bbfd3f02fc3fa0f393a46d85a5a
    
    vidc: Update from CodeAurora
    
     * Bring vidc closer to CodeAurora's latest, minus 3D support
    
    Change-Id: I4b2e09fe47ae7cad013289ec292fa0eb13a27baa

--- mm-video/vidc/vdec/Android.mk
+++ mm-video/vidc/vdec/Android.mk
@@ -20,8 +20,10 @@ libOmxVdec-def += -UENABLE_DEBUG_LOW
 libOmxVdec-def += -DENABLE_DEBUG_HIGH
 libOmxVdec-def += -DENABLE_DEBUG_ERROR
 libOmxVdec-def += -UMULTI_DEC_INST
+libOmxVdec-def += -UINPUT_BUFFER_LOG
+libOmxVdec-def += -UOUTPUT_BUFFER_LOG
 libOmxVdec-def += -DMAX_RES_720P
-
+libOmxVdec-def += -DCONFIG_MSM_MDP40
 # ---------------------------------------------------------------------------------
 # 			Make the Shared library (libOmxVdec)
 # ---------------------------------------------------------------------------------
@@ -37,7 +39,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS		:= $(libOmxVdec-def)
 LOCAL_C_INCLUDES	:= $(libmm-vdec-inc)
 
-LOCAL_SHARED_LIBRARIES	:= liblog libutils libbinder
+LOCAL_SHARED_LIBRARIES  := liblog libutils libbinder libcutils
 
 LOCAL_SRC_FILES         := src/frameparser.cpp
 LOCAL_SRC_FILES         += src/h264_utils.cpp
